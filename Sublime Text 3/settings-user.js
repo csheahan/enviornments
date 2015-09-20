d4d3df2864e0ec70d5fbea8354e755bb85b2efd3
csheahan <chris.sheahan@live.com>
2015-09-20 16:52:55 -0500

    Add sublime text 3 initial stuff

---
+++ Sublime Text 3/settings-user.js
@@ -0,0 +1,37 @@
+{
+  // Bold folders in the sidebar
+  "bold_folder_labels": true,
+
+  // Default line endings with '\n' instead of '\r\n'
+  "default_line_ending": "unix",
+
+  // Get rid of the arrows in the tabs
+  "enable_tab_scrolling": false,
+
+  // Always make block folding buttons in the gutter visible
+  "fade_fold_buttons": false,
+
+  // Size of the fonts
+  "font_size": 8,
+
+  // Highlight the current line
+  "highlight_line": true,
+
+  // Disable editing with vi keys
+  "ignored_packages":
+  [
+    "Vintage"
+  ],
+
+  // Add a visual ruler at 80 charactes
+  "rulers":
+  [
+    80
+  ],
+
+  // Size of a tab
+  "tab_size": 2,
+
+  // Tabs -> spaces
+  "translate_tabs_to_spaces": true
+}
